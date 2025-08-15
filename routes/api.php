774b0a1b7d4840efdd0593071d4463ab1558c2da
Blnzki3 <124621386+Blaineeey@users.noreply.github.com>
2025-08-16 06:27:21 +0800

    Enhance API routes documentation and formatting in api.php

--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,25 @@
 <?php
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\ProductController;
 
+/*
+|--------------------------------------------------------------------------
+| API Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register API routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "api" middleware group. Make something great!
+|
+*/
+
+// Return all products (GET /api/products)
 Route::get('/products', [ProductController::class, 'index']);
+
+// Store a new product (POST /api/products)
 Route::post('/products', [ProductController::class, 'store']);
+
+// Update a product (PUT /api/products/{id})
 Route::put('/products/{id}', [ProductController::class, 'update']);
