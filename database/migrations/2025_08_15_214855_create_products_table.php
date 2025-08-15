ccc5263afc7296e2c6b523902c50f1275f2ea4bb
Blnzki3 <124621386+Blaineeey@users.noreply.github.com>
2025-08-16 05:59:31 +0800

    Refactor migration methods for products table to remove unnecessary docblocks

--- database/migrations/2025_08_15_214855_create_products_table.php
+++ database/migrations/2025_08_15_214855_create_products_table.php
@@ -6,21 +6,19 @@ use Illuminate\Support\Facades\Schema;
 
 return new class extends Migration
 {
-    /**
-     * Run the migrations.
-     */
-    public function up(): void
+    public function up()
     {
         Schema::create('products', function (Blueprint $table) {
             $table->id();
+            $table->string('name');
+            $table->integer('quantity');
+            $table->decimal('price', 10, 2);
+            $table->decimal('total_value', 10, 2);
             $table->timestamps();
         });
     }
 
-    /**
-     * Reverse the migrations.
-     */
-    public function down(): void
+    public function down()
     {
         Schema::dropIfExists('products');
     }
